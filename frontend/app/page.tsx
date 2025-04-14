9e926aa245484d8c4ec98608c824e1443c41fdd2
Ibnul Ashir <ibnulashir42@gmail.com>
2025-04-14 09:55:49 +0600

    change

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-lime-400 text-lg">Welcome</h1>
+      <h1 className="text-lime-400 text-lg">Welcome home</h1>
     </div>
   );
 }
