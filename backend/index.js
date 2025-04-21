d4866ed831f0856b8762f7b51e65bd0c3b08eede
Ibnul Ashir <ibnulashir42@gmail.com>
2025-04-21 18:24:14 +0600

    fix

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const connectDB = require("./config/db");
 const path = require("path");
+const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const heroRoutes = require("./routes/heroRoutes");
+const serverless = require("serverless-http");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,3 +27,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Export for Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
